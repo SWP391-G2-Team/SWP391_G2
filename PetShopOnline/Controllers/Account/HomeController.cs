7c66f9beebe51af4e4037ad9d6459e509f9f1c0a
shirokuma-desu <ezorcist.dt@gmail.com>
2023-10-06 13:37:05 +0700

    Create HomeController.cs

---
+++ PetShopOnline/Controllers/Account/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShopOnline.Controllers.Account
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
