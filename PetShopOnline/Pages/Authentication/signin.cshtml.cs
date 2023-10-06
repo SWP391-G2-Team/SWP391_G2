6c3567e800e98745642e377c4cd74b8935dcd478
kurzii-0903 <quylai62@gmail.com>
2023-10-06 14:11:54 +0700

    signin

---
+++ PetShopOnline/Pages/Authentication/signin.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetShopOnline.Pages.Authentication
+{
+    public class signinModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
