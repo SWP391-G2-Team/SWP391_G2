5fa9e80727b99f50eb5c30363580d4b00c99df61
shirokuma-desu <ezorcist.dt@gmail.com>
2023-10-06 15:09:09 +0700

    Create CreateAccount.cs

---
+++ PetShopOnline/Controllers/Account/CreateAccount.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShopOnline.Controllers.Account
+{
+    public class CreateAccount : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
