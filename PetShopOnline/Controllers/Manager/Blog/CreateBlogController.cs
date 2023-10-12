c08315ad1389f6426358945c38579eea8669bb35
kurzii-0903 <quylai62@gmail.com>
2023-10-12 17:05:02 +0700

    10/12

---
+++ PetShopOnline/Controllers/Manager/Blog/CreateBlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShopOnline.Controllers.Manager.Blog
+{
+    public class CreateBlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PetShopOnline/Controllers/Manager/Blog/DetalBlogController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetShopOnline.Controllers.Manager.Blog
+{
+    public class DetalBlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
