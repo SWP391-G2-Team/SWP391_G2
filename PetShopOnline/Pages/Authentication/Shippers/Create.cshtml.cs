fec55b49117900886f35b049101d4c3c8372d607
kurzii-0903 <quylai62@gmail.com>
2023-10-13 00:50:52 +0700

    13/10

---
+++ PetShopOnline/Pages/Authentication/Shippers/Create.cshtml.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PetShop_Project_SWP391.Models;
+using System.Data;
+using System.Text.Json;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.DotNet.Scaffolding.Shared.ProjectModel;
+using Microsoft.EntityFrameworkCore;
+
+namespace PetShop_Project_SWP391.Pages.Manager.Shippers
+{
+    [Authorize(Roles = "1")]
+    public class CreateModel : PageModel
+    {
+        private readonly ProjectContext context;
+
+        public CreateModel(ProjectContext context)
+        {
+            this.context = context;
+        }
+        [BindProperty]
+        public Models.Shipper Shipper { get; set; }
+        [BindProperty]
+        public Models.Account Account { get; set; }
+        [BindProperty]
+        public Models.Employee Employee { get; set; }
+
+        [BindProperty]
+        public string EmailShipper { get; set; }
+
+        [Required(ErrorMessage = "Mật khẩu mới là bắt buộc.")]
+        [MinLength(8, ErrorMessage = "Mật khẩu phải có ít nhất {1} ký tự.")]
+        [RegularExpression("^(?!.*\\s)(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,}$", ErrorMessage = "Mật khẩu phải chứa ít nhất một chữ cái viết thường, một chữ cái viết hoa, một chữ số và một ký tự đặc biệt. Không được chứa khoảng trắng.")]
+        [BindProperty]
+        public string PasswordShipper { get; set; }
+        public async Task<IActionResult> OnGet()
+        {
+            if (HttpContext.Session.GetString("PetSession") == null)
+                return RedirectToPage("/account/singnin");
+
+            Account = JsonSerializer.Deserialize<Models.Account>(HttpContext.Session.GetString("PetSession"));
+            Employee = context.Employees.FirstOrDefault(c => c.EmployeeId == Account.EmployeeId);
+
+            return Page();
+        }
+        public async Task<IActionResult> OnPost()
+        {
+            if (string.IsNullOrEmpty(Shipper.Name) ||
+                string.IsNullOrEmpty(Shipper.Phone) ||
+                string.IsNullOrEmpty(Shipper.Address) ||
+                string.IsNullOrEmpty(Shipper.District) ||
+                string.IsNullOrEmpty(Shipper.Wards) ||
+                string.IsNullOrEmpty(Shipper.Province) ||
+                string.IsNullOrEmpty(EmailShipper) ||
+                string.IsNullOrEmpty(PasswordShipper)
+                )
+            {
+                ViewData["msgEmpty"] = "Vui lòng nhập cả tất cả thông tin bên trên";
+                return Page();
+            }
+            var acc = await context.Accounts.FirstOrDefaultAsync(a => a.Email == EmailShipper);
+            if (acc == null)
+            {
+                var shipper = new Models.Shipper()
+                {
+                    Name = Shipper.Name,
+                    Phone = Shipper.Phone,
+                    Address = Shipper.Address,
+                    District = Shipper.District,
+                    Wards = Shipper.Wards,
+                    Province = Shipper.Province
+                };
+
+                context.Shippers.Add(shipper);
+                await context.SaveChangesAsync();
+
+                var newAcc = new Models.Account()
+                {
+                    Email = EmailShipper,
+                    Password = PasswordShipper,
+                    ShipperId = shipper.ShipperId,
+                    Role = 3,
+                    IsActive = true,
+                };
+
+                context.Accounts.Add(newAcc);
+                await context.SaveChangesAsync();
+
+                ViewData["msgSuccess"] = "Thêm Shipper thành công";
+                return Page();
+            }
+            else
+            {
+                ViewData["msgEmailSame"] = "Tài khoản đã tồn tại";
+                return Page();
+            }
+        }
+    }
+}
---
+++ PetShopOnline/Pages/Authentication/Shippers/Index.cshtml.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PetShop_Project_SWP391.Models;
+using System.Drawing.Printing;
+using System.Text.Json;
+
+namespace PetShop_Project_SWP391.Pages.Manager.Shippers
+{
+    [Authorize(Roles = "1")]
+    public class IndexModel : PageModel
+    {
+        private readonly ProjectContext dBContext;
+        private readonly IConfiguration configuration;
+        public IndexModel(ProjectContext dBContext, IConfiguration configuration)
+        {
+            this.dBContext = dBContext;
+            this.configuration = configuration;
+        }
+        [BindProperty]
+        public List<Models.Account> Accounts { get; set; }
+        [BindProperty]
+        public List<Models.Shipper> Shippers { get; set; }
+        [BindProperty]
+        public @Models.Account Account { get; set; }
+        //public Employee employee { get; set; }
+        public async Task<IActionResult> OnGet(int? PageNum, string? txtSearch)
+        {
+            if (HttpContext.Session.GetString("PetSession") == null)
+            {
+                return RedirectToPage("/account/singnin");
+            }
+            else
+            {
+                if (PageNum <= 0 || PageNum is null) PageNum = 1;
+                int PageSize = Convert.ToInt32(configuration.GetValue<string>("AppSettings:PageSize"));
+
+                // Kiểm tra giá trị PageSize và gán giá trị mặc định nếu nó bằng 0
+                if (PageSize == 0)
+                {
+                    PageSize = 10; // Giá trị mặc định
+                }
+
+                //Accounts = await dBContext.Accounts.Where(x => x.Role == 3).Include(x => x.Shipper).ToListAsync();
+                Shippers = await dBContext.Shippers.ToListAsync();
+
+                if (txtSearch != null) Shippers = Shippers.Where(x => x.Name.Contains(txtSearch.Trim())).ToList();
+
+                int TotalShipper = Shippers.Count;
+
+                // Kiểm tra PageSize để đảm bảo không chia cho 0
+                int TotalPage = PageSize > 0 ? (TotalShipper / PageSize) : 0;
+                if (TotalShipper % PageSize != 0) TotalPage++;
+                ViewData["TotalPage"] = TotalPage;
+                ViewData["CurPage"] = PageNum;
+                ViewData["txtSearch"] = txtSearch;
+
+                Shippers = Shippers.Skip((int)(((PageNum - 1) * PageSize + 1) - 1)).Take(PageSize).ToList();
+
+                return Page();
+            }
+        }
+
+        public IActionResult OnGetActive(int? id)
+        {
+            if (id == null) return Redirect("~/Manager/Shippers/index");
+
+            Models.Account account = dBContext.Accounts.FirstOrDefault(x => x.AccountId == id);
+            if (account == null) return Redirect("~/Manager/Shippers/index");
+
+            if (account.IsActive == true)
+            {
+                account.IsActive = false;
+            }
+            else
+            {
+                account.IsActive = true;
+            }
+            dBContext.SaveChanges();
+
+            return Redirect("~/Manager/Shippers/index");
+        }
+    }
+}
