ffb4e8c49a812a340339679d62924f0ab915e6f5
shirokuma-desu <ezorcist.dt@gmail.com>
2023-10-13 14:06:57 +0700

    gg my teammate

--- PetShopOnline/Controllers/Manager/Blog/CreateBlogController.cs
+++ PetShopOnline/Controllers/Manager/Blog/CreateBlogController.cs
@@ -1,12 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.DotNet.Scaffolding.Shared.ProjectModel;
+using PetShopOnline.Models;
+using System.Text.Json;
 
 namespace PetShopOnline.Controllers.Manager.Blog
 {
     public class CreateBlogController : Controller
     {
-        public IActionResult Index()
+        [Authorize(Roles = "1")]
+        public class AddBlogModel : PageModel
         {
-            return View();
+            private readonly DTB_PETSHOPContext dBContext;
+            private readonly IConfiguration configuration;
+            private readonly IWebHostEnvironment _hostingEnvironment;
+
+            public AddBlogModel(DTB_PETSHOPContext dBContext, IWebHostEnvironment hostingEnvironment)
+            {
+                this.dBContext = dBContext;
+                _hostingEnvironment = hostingEnvironment;
+            }
+
+            [BindProperty]
+            public Models.BlogDetail BlogDetail { get; set; }
+
+            [BindProperty]
+            public Models.Account Account { get; set; }
+
+            [BindProperty]
+            public Models.Employee Employee { get; set; }
+
+            public async Task<IActionResult> OnGet()
+            {
+                if (HttpContext.Session.GetString("PetSession") == null)
+                    return RedirectToPage("/account/singnin");
+
+                Account = JsonSerializer.Deserialize<Models.Account>(HttpContext.Session.GetString("PetSession"));
+                Employee = dBContext.Employees.FirstOrDefault(c => c.EmployeeId == Account.EmployeeId);
+
+                return Page();
+            }
+            public async Task<IActionResult> OnPost(IFormFile imageFile)
+            {
+                if (imageFile != null && imageFile.Length > 0)
+                {
+                    Console.WriteLine(imageFile);
+                    BlogDetail.FeaturedImageUrl = "/img/" + imageFile.FileName.ToString();
+
+                    //dBContext.BlogDetails.Add(BlogDetail);
+
+                }
+
+                var today = DateTime.Today;
+
+                var blog = new BlogDetail()
+                {
+                    Content = BlogDetail.Content,
+                    Heading = BlogDetail.Heading,
+                    PageTitle = BlogDetail.PageTitle,
+                    ShortDescription = BlogDetail.ShortDescription,
+                    FeaturedImageUrl = BlogDetail.FeaturedImageUrl,
+                    PublishedDate = DateTime.Now
+                };
+
+                if (blog.PublishedDate < today || blog.PublishedDate > today) // Nếu ngày đăng nhỏ hơn ngày hôm nay, thì đặt ngày đăng bằng ngày hôm nay
+                {
+                    blog.PublishedDate = today;
+                }
+
+                dBContext.BlogDetails.Add(blog);
+                await dBContext.SaveChangesAsync();
+
+                ViewData["msgSuccess"] = "Tạo Blog thành công";
+                return RedirectToPage("/manager/Blog/List");
+
+
+            }
+
         }
     }
 }
--- PetShopOnline/Controllers/Manager/Blog/ListBlogController.cs
+++ PetShopOnline/Controllers/Manager/Blog/ListBlogController.cs
@@ -1,12 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PetShopOnline.Models;
 
 namespace PetShopOnline.Controllers.Manager.Blog
 {
     public class ListBlogController : Controller
     {
-        public IActionResult Index()
+        [Authorize(Roles = "1")]
+        public class ListModel : PageModel
         {
-            return View();
+            private readonly DTB_PETSHOPContext dBContext;
+            private readonly IConfiguration configuration;
+
+            public ListModel(DTB_PETSHOPContext dBContext, IConfiguration configuration)
+            {
+                this.dBContext = dBContext;
+                this.configuration = configuration;
+            }
+
+            [BindProperty]
+            public List<Models.Account> Accounts { get; set; }
+
+            [BindProperty]
+            public @Models.Account Account { get; set; }
+
+            [BindProperty]
+            public List<Models.BlogDetail> BlogDetails { get; set; } = default!;
+
+            public BlogDetail BlogDetail { get; set; }
+
+            public async Task<IActionResult> OnGet(int? PageNum, string? txtSearch)
+            {
+                if (HttpContext.Session.GetString("PetSession") == null)
+                {
+                    return RedirectToPage("/account/singnin");
+                }
+                else
+                {
+                    if (PageNum <= 0 || PageNum is null) PageNum = 1;
+                    int PageSize = Convert.ToInt32(configuration.GetValue<string>("AppSettings:PageSize"));
+
+                    // Kiểm tra giá trị PageSize và gán giá trị mặc định nếu nó bằng 0
+                    if (PageSize == 0)
+                    {
+                        PageSize = 3; // Giá trị mặc định
+                    }
+
+                    IQueryable<BlogDetail> query = dBContext.BlogDetails;
+
+                    if (!string.IsNullOrEmpty(txtSearch))
+                    {
+                        query = query.Where(x => x.PageTitle.Contains(txtSearch.Trim()));
+                    }
+                    query = query.OrderByDescending(x => x.BlogId);
+
+                    int TotalProduct = await query.CountAsync();
+
+                    // Kiểm tra PageSize để đảm bảo không chia cho 0
+                    int TotalPage = PageSize > 0 ? (TotalProduct / PageSize) : 0;
+                    if (TotalProduct % PageSize != 0) TotalPage++;
+                    ViewData["TotalPage"] = TotalPage;
+                    ViewData["CurPage"] = PageNum;
+                    ViewData["txtSearch"] = txtSearch;
+
+                    BlogDetails = await query.Skip((int)(PageNum - 1) * PageSize).Take(PageSize).ToListAsync();
+
+                    return Page();
+                }
+            }
+
+            public async Task<IActionResult> OnPost(int id)
+            {
+                var blog = await dBContext.BlogDetails.FindAsync(id);
+                if (blog == null)
+                {
+                    return NotFound();
+                }
+
+                dBContext.BlogDetails.Remove(blog);
+                await dBContext.SaveChangesAsync();
+
+                return RedirectToPage("/Manager/Blog/List");
+            }
         }
     }
 }
