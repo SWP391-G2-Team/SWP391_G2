df7ddbb6c93785823cdcd201fc2c328a073a5362
shirokuma-desu <ezorcist.dt@gmail.com>
2023-10-14 15:14:46 +0700

    Create Index.cshtml.cs

---
+++ PetShopOnline/Pages/Customers/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetShopOnline.Pages.Customers
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
