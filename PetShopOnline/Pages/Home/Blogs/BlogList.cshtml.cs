69a9e3a7c7734a07ff2b4387dad5497da590ea58
shirokuma-desu <ezorcist.dt@gmail.com>
2023-10-14 15:15:02 +0700

    Create BlogList.cshtml.cs

---
+++ PetShopOnline/Pages/Home/Blogs/BlogList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetShopOnline.Pages.Home.Blogs
+{
+    public class BlogListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
