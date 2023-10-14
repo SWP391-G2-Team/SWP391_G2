abcd2a6385e1dd622e193b26d073a540422c289f
shirokuma-desu <ezorcist.dt@gmail.com>
2023-10-14 22:21:44 +0700

    Create CRU for product

---
+++ PetShopOnline/Pages/Manager/Products/Create.cshtml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.DotNet.Scaffolding.Shared.ProjectModel;
+using Microsoft.EntityFrameworkCore;
+using PetShopOnline.Models;
+using System.Text.Json;
+
+namespace PetShopOnline.Pages.Manager.Products
+{
+    public class CreateModel : PageModel
+    {
+        private readonly DTB_PETSHOPContext _context;
+
+        public CreateModel(DTB_PETSHOPContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Product Product { get; set; }
+
+        [BindProperty]
+        public Models.PictureProduct1 pictureProduct { get; set; }
+
+        [BindProperty]
+        public Models.Account Account { get; set; }
+
+        [BindProperty]
+        public List<Models.Category> Category { get; set; }
+
+        [BindProperty]
+        public Models.Employee Employee { get; set; }
+
+        public async Task<IActionResult> OnGet()
+        {
+            if (HttpContext.Session.GetString("PetSession") == null)
+                return RedirectToPage("/Account/SignIn");
+
+            Account = JsonSerializer.Deserialize<Models.Account>(HttpContext.Session.GetString("PetSession"));
+            Employee = _context.Employees.FirstOrDefault(c => c.EmployeeId == Account.EmployeeId);
+            Category = await _context.Categories.ToListAsync();
+            return Page();
+        }
+        public async Task<IActionResult> OnPost(string imageLink)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(imageLink))
+                {
+                    int maxLength = 200; // Adjust the maximum length as needed
+                    pictureProduct.Picture = imageLink.Length <= maxLength ? imageLink : imageLink.Substring(0, maxLength);
+                }
+                //pictureProduct.PictureId = 0;
+                var product = new Models.Product()
+                {
+                    ProductName = Product.ProductName,
+                    QuantityPerUnit = Product.QuantityPerUnit,
+                    UnitPrice = Product.UnitPrice,
+                    Description = Product.Description,
+                    CreateDate = DateTime.Now,
+                    CategoryId = Product.CategoryId,
+                    Pictures = new List<PictureProduct1> { pictureProduct }, // Add the pictureProduct to the list of pictures
+                    Status = true,
+                    Discontinued = false
+                };
+
+                _context.Products.Add(product);
+                await _context.SaveChangesAsync();
+
+                ViewData["msgSuccess"] = "Thêm sản phẩm thành công";
+                return RedirectToPage("/Manager/Products/Index");
+            }
+            catch (Exception e)
+            {
+                return RedirectToPage("/Manager/Products/Index");
+            }
+        }
+    }
+}
+}
---
+++ PetShopOnline/Pages/Manager/Products/Detail.cshtml.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.DotNet.Scaffolding.Shared.ProjectModel;
+using Microsoft.EntityFrameworkCore;
+using PetShopOnline.Models;
+using System.Text.Json;
+
+namespace PetShopOnline.Pages.Manager.Products
+{
+    [Authorize(Roles = "1")]
+    public class DetailModel : PageModel
+    {
+            private readonly DTB_PETSHOPContext _context;
+            public DetailModel(DTB_PETSHOPContext context)
+            {
+                _context = context;
+            }
+
+            public Models.Product Product { get; set; }
+            [BindProperty]
+            public @Models.Account Account { get; set; }
+            [BindProperty]
+            public Customer Customer { get; set; }
+            public Models.Employee Employee { get; set; }
+            public async Task<IActionResult> OnGetAsync(int? id)
+            {
+                if (HttpContext.Session.GetString("PetSession") == null) return RedirectToPage("/Account/SignIn");
+
+                Account = JsonSerializer.Deserialize<@Models.Account>(HttpContext.Session.GetString("PetSession"));
+                //Customer = dBContext.Customers.FirstOrDefault(c => c.CustomerId == Account.CustomerId);
+
+                Employee = _context.Employees.FirstOrDefault(c => c.EmployeeId == Account.EmployeeId);
+                if (id == null || _context.Products == null)
+                {
+                    return NotFound();
+                }
+                var product = await _context.Products.Include(x => x.Pictures)
+                    .Include(p => p.Category).FirstOrDefaultAsync(c => c.ProductId == id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    Product = product;
+                }
+                return Page();
+            }
+        }
+    }
+
