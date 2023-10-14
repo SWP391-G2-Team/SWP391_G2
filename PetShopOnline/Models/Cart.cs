ad304e81e55f7ed51b7c8ae78a6bbef64c5d4736
shirokuma-desu <ezorcist.dt@gmail.com>
2023-10-14 15:23:24 +0700

    Create Cart.cs

---
+++ PetShopOnline/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿namespace PetShopOnline.Models
+{
+    public class Cart
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public Cart()
+        {
+
+        }
+        public Cart(Product product, int quantity)
+        {
+            Product = product;
+            Quantity = quantity;
+        }
+    }
+}
