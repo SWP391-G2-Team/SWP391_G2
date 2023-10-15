587875978dc9b69665d5637b33f4c0446b4ab4d4
shirokuma-desu <ezorcist.dt@gmail.com>
2023-10-15 14:14:19 +0700

    Create Profile Manager

---
+++ PetShopOnline/Pages/Manager/Profile/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PetShopOnline.Pages.Manager.Profile
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
